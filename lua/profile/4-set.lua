b646e984af1162c14255479ee2bb77a8fc4667ba
Lightja <21020214+Lightja@users.noreply.github.com>
2025-03-07 23:23:33 -0500

    reverted nobackup addition, didnt work as expected, will work on just finding a way to exclude these files from workspace and git repos.

--- lua/profile/4-set.lua
+++ lua/profile/4-set.lua
@@ -3,7 +3,6 @@ vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.nobackup = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
